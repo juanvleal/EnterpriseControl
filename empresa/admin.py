f037561d58d13837d2f93d8a02014174b2d04e5e
juanvleal <juanvleal@gmail.com>
2023-04-13 11:59:12 -0300

    Criando models e exibindo no Admin.

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Departamento, Funcionario, Projeto, ProjetoFuncionario
 
-# Register your models here.
+class FuncionarioInline(admin.TabularInline):
+    model = ProjetoFuncionario
+    extra = 1
+
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'horas_necessarias', 'prazo_estimado', 'horas_realizadas', 'ultimo_calculo_horas', 'departamento', 'supervisor')
+    search_fields = ('nome', 'departamento__nome')
+    list_filter = ('departamento', 'supervisor')
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'rg', 'sexo', 'data_nascimento', 'habilitacao', 'salario', 'carga_horaria_semanal', 'departamento')
+    search_fields = ('nome', 'cpf', 'rg', 'departamento__nome')
+    list_filter = ('sexo', 'habilitacao', 'departamento')
+    inlines = [FuncionarioInline]
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+admin.site.register(Departamento, DepartamentoAdmin)
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Projeto, ProjetoAdmin)
--- empresa/models.py
+++ empresa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11, unique=True)
+    rg = models.CharField(max_length=20, unique=True)
+    sexo = models.CharField(max_length=10)
+    data_nascimento = models.DateField()
+    habilitacao = models.BooleanField()
+    salario = models.DecimalField(max_digits=8, decimal_places=2)
+    carga_horaria_semanal = models.IntegerField()
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+class Projeto(models.Model):
+    nome = models.CharField(max_length=50)
+    horas_necessarias = models.IntegerField()
+    prazo_estimado = models.DateField()
+    horas_realizadas = models.IntegerField(default=0)
+    ultimo_calculo_horas = models.DateField()
+    supervisor = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+class ProjetoFuncionario(models.Model):
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    horas_trabalhadas = models.IntegerField()
+    horas_supervisao = models.IntegerField()
